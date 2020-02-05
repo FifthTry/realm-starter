d15f578c4156177e6e4de381e7d092c8a036be9e
Amit Upadhyay <upadhyay@gmail.com>
2020-02-05 21:02:01 +0530

    minor formatting etc

--- frontend/Pages/Index.elm
+++ frontend/Pages/Index.elm
@@ -1,4 +1,4 @@
-module Pages.Index exposing (Config, app, main)
+module Pages.Index exposing (main)
 
 import Browser as B
 import Element as E
@@ -6,6 +6,10 @@ import Element.Events as EE
 import Element.Font as EF
 import Json.Decode as JD
 import Realm as R
+
+
+main =
+    R.app app
 
 
 app : R.App Config Config ()
@@ -22,11 +26,15 @@ type alias Config =
 view : Config -> E.Element (R.Msg ())
 view c =
     E.column [ E.centerX, E.centerY, E.spacing 20 ]
-        [ E.el [ E.centerX, EF.italic ] (E.text "Hello Elm")
+        [ E.el [ E.centerX, EF.italic ]
+            (E.text "Hello Elm")
         , E.text ("Message from server: " ++ c.message)
         , E.row [ E.spacing 10, E.alignRight ]
             [ E.text ("Count: " ++ String.fromInt c.count)
-            , E.link [] { url = "/increment/", label = E.text "Go Up" }
+            , E.link []
+                { url = "/increment/"
+                , label = E.text "Go Up"
+                }
             ]
         ]
 
@@ -41,7 +49,3 @@ config =
     JD.map2 Config
         (JD.field "message" JD.string)
         (JD.field "count" JD.int)
-
-
-main =
-    R.app app
