b5fe13fa75d47899b114d248f2d4d1b0eda19c4c
Amit Upadhyay <upadhyay@gmail.com>
2020-02-08 13:06:38 +0530

    storybook fixes from realm

--- frontend/Storybook.elm
+++ frontend/Storybook.elm
@@ -13,7 +13,11 @@ stories : List ( String, List Story )
 stories =
     [ ( "Index"
       , [ index "count_42" "Count = 42" { message = "hello world", count = 42 }
-        , index "big_message" "Big Message" { message = "hello world", count = 42 }
+        , index "big_message"
+            "Big Message"
+            { message = "hello world, its really nice to meet you!"
+            , count = 2
+            }
         ]
       )
     ]
